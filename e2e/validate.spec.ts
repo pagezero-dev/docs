2b33a902b77ae3ed4fb77e6e2fc869aea850e6e0
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2025-08-12 09:20:13 +1000

    Update e2e tests

--- e2e/validate.spec.ts
+++ e2e/validate.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test"
 
 test("has content", async ({ page }) => {
   await page.goto("http://localhost:3000")
-  await expect(page.getByText("Welcome to Starlight")).toBeVisible()
+  await expect(page.getByText("PageZero Documentation")).toBeVisible()
 })
