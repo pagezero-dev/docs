7450e88f4701aa650269877f0aee26525a315bab
Paweł Gałązka <829242+pawelgalazka@users.noreply.github.com>
2026-01-17 14:06:50 +1100

    Adjust e2e tests

--- e2e/validate.spec.ts
+++ e2e/validate.spec.ts
@@ -2,5 +2,7 @@ import { expect, test } from "@playwright/test"
 
 test("has content", async ({ page }) => {
   await page.goto("http://localhost:3000")
-  await expect(page.getByText("PageZero Documentation")).toBeVisible()
+  await expect(
+    page.getByRole("heading", { name: "PageZERO Docs" }),
+  ).toBeVisible()
 })
